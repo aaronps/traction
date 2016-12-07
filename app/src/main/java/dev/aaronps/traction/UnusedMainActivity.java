f89f1da826204e98449afd7ee5739da420a101cb
aaronps <aaronperezsanchez@hotmail.com>
2016-12-07 12:14:38 +0800

    Added AndroidStudio project

---
+++ app/src/main/java/dev/aaronps/traction/UnusedMainActivity.java
@@ -0,0 +1,12 @@
+package dev.aaronps.traction;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UnusedMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
